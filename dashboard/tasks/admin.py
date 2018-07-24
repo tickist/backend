b54a4ff3a51637da297273174688c6fd12eb5c42
Bartlomiej Borzucki <borzucki@gmail.com>
2018-07-25 00:09:03 +0200

    Add TaskStatistics TaskStep and Tag to the django-admin

--- dashboard/tasks/admin.py
+++ dashboard/tasks/admin.py
@@ -1,5 +1,8 @@
 #-*- coding: utf-8 -*-
 from django.contrib import admin
-from dashboard.tasks.models import Task
+from dashboard.tasks.models import Task, TaskStatistics, TaskStep, Tag
 
 admin.site.register(Task)
+admin.site.register(TaskStatistics)
+admin.site.register(TaskStep)
+admin.site.register(Tag)
